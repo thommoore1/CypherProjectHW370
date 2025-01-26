a0aef6bf5fc74fa6068e0b47d0b38282f1d322be
Tom Moore <thommoore@chapman.edu>
2025-01-25 18:15:24 -0800

    Let's see what happens

---
+++ CypherUnit Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CypherUnit_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
